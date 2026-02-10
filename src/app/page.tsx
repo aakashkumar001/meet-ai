ee23ff73148b5a78eaec38ec71f367d8037be896
aakashkr <aspakash000@gmail.com>
2026-02-10 21:02:03 +0530

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="">
+      <h1 className="text-3xl font-bold text-green-700">Hey Meet AI</h1>
+    </div>
+  );
+}
