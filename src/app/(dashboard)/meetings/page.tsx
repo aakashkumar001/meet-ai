76fd8b334ade80a36c610100cbd347a532c18bae
aakashkr <aspakash000@gmail.com>
2026-02-13 23:47:33 +0530

    meeting-pages

---
+++ src/app/(dashboard)/meetings/page.tsx
@@ -0,0 +1,22 @@
+import { MeetingErrorLoading, MeetingsView, MeetingViewLoading } from "@/modules/meetings/ui/views/meetings-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+const Page = () => {
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<MeetingViewLoading/>}>
+        <ErrorBoundary fallback={<MeetingErrorLoading/>}>
+          <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
+  );
+};
+
+export default Page;
